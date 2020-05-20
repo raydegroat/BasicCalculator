2287d57de0b4ddae4dff4a47ce4551a26bd781d0
raydegroat <stockholmray@gmail.com>
2020-05-20 10:34:10 +0200

    Third commit - added user definable operation

--- BasicCalculator.cs
+++ BasicCalculator.cs
@@ -7,14 +7,36 @@ namespace BasicCalculatorAlligator
         static void Main(string[] args)
         {
 
-            int num1;
-            int num2;
+            int num1, num2;
+            char operation;
 
             Console.WriteLine("Enter an integer between -2 billion and +2 billion");
             num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter another integer between -2 billion and +2 billion");
             num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            Console.WriteLine("Enter one of the following 4 operators + - * /");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine();
+
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+                    break;
+                case '-':
+                    Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+                    break;
+                case '*':
+                    Console.WriteLine(num1 + " x " + num2 + " = " + (num1 * num2));
+                    break;
+                case '/':
+                    Console.WriteLine(num1 + " ÷ " + num2 + " = " + (num1 / num2));
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
         }   
     }
 }
